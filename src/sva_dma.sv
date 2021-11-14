d545b0447a859ad66de3ecb4d209a75a68ca04b3
PratikNarkhede20 <pratik2@pdx.edu>
2021-11-14 13:37:35 -0800

    added DACK cover in sva_dma

--- src/sva_dma.sv
+++ src/sva_dma.sv
@@ -3,6 +3,9 @@ module dma_checker_sva(busInterface busIf);
 
 default clocking c0 @(posedge busIf.CLK); endclocking
 
-
+DACK0isOne_c : cover property (busIf.DACK[0] == 1'b1);
+DACK1isOne_c : cover property (busIf.DACK[1] == 1'b1);
+DACK2isOne_c : cover property (busIf.DACK[2] == 1'b1);
+DACK3isOne_c : cover property (busIf.DACK[3] == 1'b1);
 
 endmodule
