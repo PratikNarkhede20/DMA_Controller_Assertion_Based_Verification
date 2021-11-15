1f9a1438d0ee1f64b3ca23045891eeface24c86f
PratikNarkhede20 <pratik2@pdx.edu>
2021-11-14 21:44:31 -0800

    changes in sva_dma

--- src/sva_dma.sv
+++ src/sva_dma.sv
@@ -3,15 +3,18 @@ module dma_checker_sva(busInterface busIf);
 
 default clocking c0 @(posedge busIf.CLK); endclocking
 
-DREQ0isOne_c : cover property (busIf.DREQ[0] == 1'b1);
-DREQ1isOne_c : cover property (busIf.DREQ[1] == 1'b1);
-DREQ2isOne_c : cover property (busIf.DREQ[2] == 1'b1);
-DREQ3isOne_c : cover property (busIf.DREQ[3] == 1'b1);
+DREQ0isOne_c : cover property (busIf.DREQ == 4'b0001);
+DREQ1isOne_c : cover property (busIf.DREQ == 4'b0010);
+DREQ2isOne_c : cover property (busIf.DREQ == 4'b0100);
+DREQ3isOne_c : cover property (busIf.DREQ == 4'b1000);
 
-DACK0isOne_c : cover property (busIf.DACK[0] == 1'b1);
-DACK1isOne_c : cover property (busIf.DACK[1] == 1'b1);
-DACK2isOne_c : cover property (busIf.DACK[2] == 1'b1);
-DACK3isOne_c : cover property (busIf.DACK[3] == 1'b1);
+DACK0isOne_c : cover property (busIf.DACK == 4'b0001);
+DACK1isOne_c : cover property (busIf.DACK == 4'b0010);
+DACK2isOne_c : cover property (busIf.DACK == 4'b0100);
+DACK3isOne_c : cover property (busIf.DACK == 4'b1000);
+
+iorIsOne_c : cover property (busIf.IOR = 1'b1);
+iowIsOne_c : cover property (busIf.IOW = 1'b1);
 
 
 endmodule
