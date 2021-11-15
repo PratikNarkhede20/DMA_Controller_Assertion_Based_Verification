c25ad5a85218b14723f4455b3d84c90d9936ff2a
PratikNarkhede20 <pratik2@pdx.edu>
2021-11-14 21:39:10 -0800

    update

--- src/sva_dma.sv
+++ src/sva_dma.sv
@@ -8,7 +8,10 @@ DREQ1isOne_c : cover property (busIf.DREQ[1] == 1'b1);
 DREQ2isOne_c : cover property (busIf.DREQ[2] == 1'b1);
 DREQ3isOne_c : cover property (busIf.DREQ[3] == 1'b1);
 
-DREQ3isOne_c : cover property (busIf.DREQ[3] == 1'b1);
+DACK0isOne_c : cover property (busIf.DACK[0] == 1'b1);
+DACK1isOne_c : cover property (busIf.DACK[1] == 1'b1);
+DACK2isOne_c : cover property (busIf.DACK[2] == 1'b1);
+DACK3isOne_c : cover property (busIf.DACK[3] == 1'b1);
 
 
 endmodule
