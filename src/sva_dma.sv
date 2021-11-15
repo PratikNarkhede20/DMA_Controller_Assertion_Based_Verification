942134277e9044afed50b5ae17abd217ea61b9a6
PratikNarkhede20 <pratik2@pdx.edu>
2021-11-14 22:58:38 -0800

    edit

--- src/sva_dma.sv
+++ src/sva_dma.sv
@@ -3,16 +3,19 @@ module dma_checker_sva(busInterface busIf);
 
 default clocking c0 @(posedge busIf.CLK); endclocking
 
+//cover for Data request
 DREQ0isOne_c : cover property (busIf.DREQ == 4'b0001);
 DREQ1isOne_c : cover property (busIf.DREQ == 4'b0010);
 DREQ2isOne_c : cover property (busIf.DREQ == 4'b0100);
 DREQ3isOne_c : cover property (busIf.DREQ == 4'b1000);
 
+//cover for data acknowledgement
 DACK0isOne_c : cover property (busIf.DACK == 4'b0001);
 DACK1isOne_c : cover property (busIf.DACK == 4'b0010);
 DACK2isOne_c : cover property (busIf.DACK == 4'b0100);
 DACK3isOne_c : cover property (busIf.DACK == 4'b1000);
 
+//cover for input output read or write signal from timing and control
 iorIsActive_c : cover property (busIf.IOR_N == 1'b0);
 iowIsActive_c : cover property (busIf.IOW_N == 1'b0);
 
