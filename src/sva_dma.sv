7ce89a7a57c0f16f3faf4d21163e1218ae0321a5
PratikNarkhede20 <pratik2@pdx.edu>
2021-11-28 12:24:26 -0800

    updated sva_dma

--- src/sva_dma.sv
+++ src/sva_dma.sv
@@ -4,21 +4,21 @@ module dma_checker_sva(busInterface busIf);
 default clocking c0 @(posedge busIf.CLK); endclocking
 
 //cover for Data request
-DREQ0isOne_c : cover property (busIf.DREQ == 4'b0001);
-DREQ1isOne_c : cover property (busIf.DREQ == 4'b0010);
-DREQ2isOne_c : cover property (busIf.DREQ == 4'b0100);
-DREQ3isOne_c : cover property (busIf.DREQ == 4'b1000);
+//DREQ0isOne_c : cover property (busIf.DREQ == 4'b0001);
+//DREQ1isOne_c : cover property (busIf.DREQ == 4'b0010);
+//DREQ2isOne_c : cover property (busIf.DREQ == 4'b0100);
+//DREQ3isOne_c : cover property (busIf.DREQ == 4'b1000);
 
 //cover for data acknowledgement
 DACK0isOne_c : cover property (busIf.DACK == 4'b0001);
-DACK1isOne_c : cover property (busIf.DACK == 4'b0010);
-DACK2isOne_c : cover property (busIf.DACK == 4'b0100);
-DACK3isOne_c : cover property (busIf.DACK == 4'b1000);
+//DACK1isOne_c : cover property (busIf.DACK == 4'b0010);
+//DACK2isOne_c : cover property (busIf.DACK == 4'b0100);
+//DACK3isOne_c : cover property (busIf.DACK == 4'b1000);
 
 //cover for input output read or write signal from timing and control
 iorIsActive_c : cover property (busIf.IOR_N == 1'b0);
 iowIsActive_c : cover property (busIf.IOW_N == 1'b0);
 
-stateTransistion_a: assert property((busIf.CS_N && dma.tC.state == dma.tC.state.S0) |=> (dma.tC.nextState == dma.tC.state.S1) );
+stateTransistion_a: assert property((busIf.CS_N && check1.tC.state == check1.tC.state.SO) |=> (check1.tC.nextState == check1.tC.state.S1) );
 
 endmodule
