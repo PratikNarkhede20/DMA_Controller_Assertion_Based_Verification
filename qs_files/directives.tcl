7987d06553daee1bedfdcf548e883ec2335e5760
PratikNarkhede20 <pratik2@pdx.edu>
2021-12-07 17:33:19 -0800

    .

--- qs_files/directives.tcl
+++ qs_files/directives.tcl
@@ -10,7 +10,13 @@ netlist clock busIf.CLK -period 10
 #formal netlist constraint dma.intRegIf.modeReg[2] 2'b01
 #formal netlist constraint dma.intRegIf.modeReg[3] 2'b01
 
-formal netlist constraint busIf.RESET 1'b1
-after 3000
+#formal netlist constraint busIf.RESET 1'b1
+#after 3000
+#formal netlist constraint busIf.RESET 1'b0
+
+
+set busIf.RESET 1'b1
+after 300
 formal netlist constraint busIf.RESET 1'b0
+
 netlist blackbox priorityLogic
