92494f27b975fc829a54c3028b675b40bc560e6f
Pratik Avinash Narkhede <pratik2@mo.ece.pdx.edu>
2021-12-08 23:23:43 -0800

    push

--- Output_Results/qverify_cmds.tcl
+++ Output_Results/qverify_cmds.tcl
@@ -1,7 +1,6 @@
 # do qs_files/directives.tcl
 netlist clock busIf.CLK -period 10
 formal netlist constraint busIf.RESET 1'b0
-netlist cutpoint dma.d.ioDataBuffer
 # end do qs_files/directives.tcl
 formal compile -d dma -cuname my_bind_sva -target_cover_statements
 formal verify -init qs_files/myinit.init -timeout 5m
